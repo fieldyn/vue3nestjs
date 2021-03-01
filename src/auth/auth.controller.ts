4c522395c878389deecc4d694c0bed886400e81e
Alberto Biancardi <fieldyn@gmail.com>
2021-03-01 00:33:04 -0500

    10 validations

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,11 +1,23 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { BadRequestException, Body, Controller, Post } from '@nestjs/common';
 import { UserService } from 'src/user/user.service';
+import * as bcrypt from 'bcrypt';
+import { RegisterDto } from './models/register.dto';
 
 @Controller()
 export class AuthController {
   constructor(private userService: UserService) {}
   @Post('register')
-  async register(@Body() body) {
-    return this.userService.create(body);
+  async register(@Body() body: RegisterDto) {
+    if (body.password !== body.password_confirm) {
+      throw new BadRequestException('Passwords do not match!');
+    }
+
+    const hashed = await bcrypt.hash(body.password, 12);
+    return this.userService.create({
+      first_name: body.first_name,
+      last_name: body.last_name,
+      email: body.email,
+      password: hashed,
+    });
   }
 }
