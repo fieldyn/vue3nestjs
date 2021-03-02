8d6bf9a5dcf6ef86ca457b19355e2149cff4f7c0
Alberto Biancardi <fieldyn@gmail.com>
2021-03-01 22:50:47 -0500

    12

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -4,14 +4,20 @@ import {
   Controller,
   NotFoundException,
   Post,
+  Res,
 } from '@nestjs/common';
 import { UserService } from 'src/user/user.service';
 import * as bcrypt from 'bcrypt';
 import { RegisterDto } from './models/register.dto';
+import { JwtService } from '@nestjs/jwt';
+import { Response } from 'express';
 
 @Controller()
 export class AuthController {
-  constructor(private userService: UserService) {}
+  constructor(
+    private userService: UserService,
+    private jwtService: JwtService,
+  ) {}
   @Post('register')
   async register(@Body() body: RegisterDto) {
     if (body.password !== body.password_confirm) {
@@ -31,6 +37,7 @@ export class AuthController {
   async login(
     @Body('email') email: string,
     @Body('password') password: string,
+    @Res({ passthrough: true }) response: Response,
   ) {
     const user = await this.userService.findOne({ email });
 
@@ -42,6 +49,9 @@ export class AuthController {
       throw new BadRequestException('Invalid credentials');
     }
 
+    const jwt = await this.jwtService.signAsync({ id: user.id });
+
+    response.cookie('jwt', jwt, { httpOnly: true });
     return user;
   }
 }
